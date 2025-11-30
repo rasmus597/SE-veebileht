989a9003e2902d3ed5d28edbc6c926043909935b
unknown <jaagupsu@ut.ee>
2025-11-30 22:04:54 +0200

    Paar viimistlust

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 function andmetüüp(evt, tüüp) {
   var i, tabcontent, tablinks;
-
+  
+  //Automaatselt peidab kõik elemendid, mille klass on "sakksisu"
   tabcontent = document.getElementsByClassName("sakksisu");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
+  //Leiab kõik elemendid, mille klass on "sakklink" ja võtab neilt "active" märgise
   tablinks = document.getElementsByClassName("sakklink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
+ //Näitab antud saki infot ning määrab ta aktiivseks
   document.getElementById(tüüp).style.display = "block";
   evt.currentTarget.className += " active";
-
-  document.getElementById(tüüp).click();
 } 
 
 //Kood ei ole minu kirjutatud! Autoriks on W3Schools, kood saadud lehelt https://www.w3schools.com/howto/howto_js_tabs.asp
